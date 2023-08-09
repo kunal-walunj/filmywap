bd464445fcc1ee4939554900cd6268e2f9e041a5
kunal-walunj <128592643+kunal-walunj@users.noreply.github.com>
2023-08-09 12:35:08 +0530

    update

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ import Start from './src/screen/Start';
 import Info from './src/components/Info';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator();v  v
 
 export default function App() {
 
